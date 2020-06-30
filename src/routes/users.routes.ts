e46f820b442ec5000b60dadd6fdda13ad62845a7
Gustavo Granato <gustavo.granato@hotmail.com>
2020-06-30 19:33:24 -0300

    create class error

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -35,19 +35,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+    delete user.password;
 
-      delete user.password;
-      return response.json(user);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(user);
   },
 );
 export default usersRouter;
