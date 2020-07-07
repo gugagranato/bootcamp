d9b307a607d145db123c230cadcd9d3a8a117771
Gustavo Granato <gustavo.granato@hotmail.com>
2020-07-06 21:37:06 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 import bodyParser from 'body-parser';
@@ -12,6 +13,8 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use('/files', express.static(uploadConfig.directory));
